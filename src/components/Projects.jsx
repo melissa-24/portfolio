a0e495c7a413d320fb580d49429cbc5e36aca987
Melissa Longenberger <242424+melissa-24@users.noreply.github.com>
2020-05-15 02:18:20 -0400

    final updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,13 +12,33 @@ function Projects() {
         </nav>
       </div>
       <div className="section-header" id="current">
-        <h3>Current Projects<a href="#top"><img src="http://images.craftsnherbs.com/top.jpg" /></a></h3>
+        <h2>Current Projects<a href="#top"><img src="http://images.craftsnherbs.com/top.jpg" /></a></h2>
       </div>
       <div className="profile-text">
-     
+        <h3>These Projects are current and ongoing.  Feel free to click on the pictures to visit the sites</h3>
+        <div className="project-card">
+          <h4>1st Collaberation Build - My job was UI</h4>
+          <a href="https://water-my-plants-ui.netlify.app/" target="_blank"><img src="http://images.craftsnherbs.com/portfolio/watermyplants.jpg" /></a>
+        </div>
+        <div className="project-card">
+          <h4>2nd Collaberation Build - My job was UI</h4>
+          <a href="https://re-mix.netlify.app/" target="_blank"><img src="http://images.craftsnherbs.com/portfolio/remix.jpg" /></a>
+        </div>
+        <div className="project-card">
+          <h4>1st Professional Build - Updated Bi-Weekly</h4>
+          <a href="http;//lah-photpgraphy.com" target="_blank"><img src="http://images.craftsnherbs.com/portfolio/lah.jpg" /></a>
+        </div>
+        <div className="project-card">
+          <h4>Personal Crocheting Website</h4>
+          <a href="http://honeybeescrochet.com" target="_blank"><img src="http://images.craftsnherbs.com/portfolio/crochet01.jpg" /></a>
+        </div>
+        <div className="project-card">
+          <h4>A Web Developers Guide Site - Still adding Contributors and content</h4>
+          <a href="https://react-help-guide.netlify.app/" target="_blank"><img src="http://images.craftsnherbs.com/portfolio/guide.jpg" /></a>
+        </div>
       </div>
       <div className="section-header" id="past">
-          <h3>Past Projects<a href="#top"><img src="http://images.craftsnherbs.com/top.jpg" /></a></h3>
+          <h2>Past Projects<a href="#top"><img src="http://images.craftsnherbs.com/top.jpg" /></a></h2>
       </div>
       <div className="profile-text">
         <h3>Many of the projects here are no longer active, they are simply a picture of what they were</h3>
@@ -29,6 +49,10 @@ function Projects() {
         <div className="project-card">
           <h4>Same site a few years later</h4>
           <img src="http://images.craftsnherbs.com/portfolio/irishgardens02.png" />
+        </div>
+        <div className="project-card">
+          <h4>5 years later same site</h4>
+          <img src="http://images.craftsnherbs.com/portfolio/irishgardens03.png" />
         </div>
       </div>
     </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,6 +6,7 @@ function Skills() {
     <div className="profile-wrapper">
       <div className="profile-header" id="top">
         <h1>Virtual Resume</h1>
+
         <nav>
             <a href="#skills">Skills Section</a>
             <a href="#history">Work History Section</a>
@@ -13,19 +14,48 @@ function Skills() {
         </nav>
       </div>
       <div className="section-header" id="skills">
-        <h3>Skills<a href="#top"><img src="http://images.craftsnherbs.com/top.jpg" /></a></h3>
+        <h2>Skills<a href="#top"><img src="http://images.craftsnherbs.com/top.jpg" /></a></h2>
       </div>
       <div className="profile-text">
+        <div className="list">
+          <ul>
+            <li>HTML5</li>
+            <li>CSS</li>
+            <li>JS</li>
+            <li>LESS</li>
+          </ul>
+          <ul>
+            <li>React</li>
+            <li>Cisco</li>
+            <li>Networking</li>
+            <li>Node.js</li>
+          </ul>
+        </div>
       </div>
       <div className="section-header" id="history">
-        <h3>Work History<a href="#top"><img src="http://images.craftsnherbs.com/top.jpg" /></a></h3>
+        <h2>Work History<a href="#top"><img src="http://images.craftsnherbs.com/top.jpg" /></a></h2>
       </div>
       <div className="profile-text">
+        <div className="list">
+          <ul>
+            <li><b>2018 - Present: </b>Freelance Webdesigner</li>
+            <li><b>2018 - Present: </b>Entrepreneur - HoneyBee's Crochet</li>
+            <li><b>2015 - 2017: </b>Web.com - Universall Technition</li>
+            <li><b>1998 - 2009: </b>US Navy - FireControlman (Weapons Specialist and Electronics)</li>
+          </ul>
+        </div>
       </div>
       <div className="section-header" id="education">
-        <h3>Enducation<a href="#top"><img src="http://images.craftsnherbs.com/top.jpg" /></a></h3>
+        <h2>Education<a href="#top"><img src="http://images.craftsnherbs.com/top.jpg" /></a></h2>
       </div>
       <div className="profile-text">
+        <div className="list">
+          <ul>
+            <li><b>2020 - Present: </b>Lambda - Full Stack Web Developer</li>
+            <li><b>2019 - Present: </b>NexGenT - Full Stack Network Engineer</li>
+            <li><b>2010 - 2012: </b>Dutchess Community College - AS in Bussiness Administration</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
